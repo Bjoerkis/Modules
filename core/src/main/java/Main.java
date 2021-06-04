bbe3e1153c30e48e6d4443d516aaf84eab7264d1
Bjoerkis <75326563+Bjoerkis@users.noreply.github.com>
2021-06-04 11:32:09 +0200

    Server(core) + client

--- core/src/main/java/Main.java
+++ core/src/main/java/Main.java
@@ -1,14 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
 
     public static void main(String[] args) {
-
-        var utils = new Utils();
-
-        System.out.println(utils.message());
-    }
+        //localhost 127.0.0.1
+        //178.174.162.51//Blombergs computer
 
 
-    private static Utils giveMeAnObject(){
-        return new Utils();
+        try (ServerSocket serverSocket = new ServerSocket(5050)) {
+
+            while(true) {
+                Socket client = serverSocket.accept();
+                System.out.println(client.getInetAddress());
+                var inputFromClient = new BufferedReader(new InputStreamReader((client.getInputStream())));
+
+                inputFromClient.lines().forEach(System.out::println);
+                inputFromClient.close();
+                client.close();
+            }
+            // String input = "";
+            // while (input != null) {
+            // input = inputFromClient.readLine();
+            // System.out.println(input);
+            // }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
